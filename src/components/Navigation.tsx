45339957fb23c5f7290d1127264eb608941e00be
Dmitrii <stepanov.dmitrii.spo@gmail.com>
2024-05-07 14:22:55 +0400

    clean store

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,6 @@ export default function FavoritesPage() {
 	return (
 		<nav className='flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white'>
 			<h3 className='font-bold'>GitHub Search</h3>
-
 			<span>
 				<Link to='/github-search/' className='mr-2'>
 					Home
